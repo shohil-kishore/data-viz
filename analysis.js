9a1a07e63c778f8601bea3b88c049b7d71d1ad69
shohil-kishore <s.kishore@auckland.ac.nz>
2019-12-11 17:02:25 +1300

    Created initial graph

---
+++ analysis.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+var data = fs.readFileSync("./count-without-auckland.json");
+var json = JSON.parse(data);
+var len = Object.keys(json.results).length;
+
+var resultsArr = [];
+for (let i = 0; i < len; i++) {
+  resultsArr.push(json.results[i].count);
+}
+
+var dateArr = [];
+for (let i = 0; i < len; i++) {
+  dateArr.push(json.results[i].timePeriod);
+}
+
+console.log(resultsArr);
+console.log(dateArr);
